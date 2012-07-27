927191ca57e4ddf936299e701cfdf2c66046fd73
Tomas Gustavsson <tomplast@gmail.com>
2012-07-27 13:31:17 +0200

    .

--- topqt.py
+++ topqt.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
 
-
-#=)
-
-import subprocess, sys, os
+import subprocess, sys, os, time
+from threading import Thread
 from PyQt4 import QtCore, QtGui, uic
 
 window = None
 columnKeyBindings = None
+
+class RefresherThread(QtCore.QThread):
+    refreshFrequency = None
+    
+    def __init__(self, refreshFrequency):
+        self.refreshFrequency = refreshFrequency
+        QtCore.QThread.__init__(self)
+
+    def run(self):
+        while True:
+            window.tableWidget.clearContents()
+            for i in range(window.tableWidget.rowCount()-1, -1, -1):
+                window.tableWidget.removeRow(i)
+            getAndShowProcesses()
+            time.sleep(self.refreshFrequency)
+
 
 def setupTable(data):
     global columnKeyBindings
@@ -17,6 +31,7 @@ def setupTable(data):
     tableWidget.setColumnCount(len(columnKeyBindings))
     for binding in columnKeyBindings:
         tableWidget.setHorizontalHeaderItem(columnKeyBindings.index(binding), QtGui.QTableWidgetItem(binding["caption"]))
+
 
 def insertRow(data):
     global columnKeyBindings
@@ -39,6 +54,22 @@ def insertRow(data):
             tableWidget.setItem(tableWidget.rowCount()-1, searchIndex-1, QtGui.QTableWidgetItem(data[matchKey]))
 
 
+def getAndShowProcesses():
+    psAUX = subprocess.getoutput('ps aux')
+    psTuple = {}
+    for line in psAUX.split("\t"):
+        columnHeadings = (line.split("\n")[0].split())
+        values = line.split("\n")[1:]
+        
+        for colValues in values:
+            for colNameNr in range(len(columnHeadings)):
+                psTuple[columnHeadings[colNameNr]] = colValues.split()[colNameNr]
+            
+            #print(psTuple)
+            psTuple["NAME"] = psTuple["COMMAND"].split("/")[-1]
+            insertRow(psTuple)
+
+
 def main():
     global window
     app = QtGui.QApplication(sys.argv)
@@ -52,19 +83,11 @@ def main():
     
     setupTable([{"caption": "PID", "key": "PID"},{"caption":"Name","key":"NAME"}, {"caption":"%CPU","key":"%CPU"},{"caption":"path","key":"COMMAND"}])
     
-    psAUX = subprocess.getoutput('ps aux')
-    psTuple = {}
-    for line in psAUX.split("\t"):
-        columnHeadings = (line.split("\n")[0].split())
-        values = line.split("\n")[1:]
-        
-        for colValues in values:
-            for colNameNr in range(len(columnHeadings)):
-                psTuple[columnHeadings[colNameNr]] = colValues.split()[colNameNr]
-            
-            print(psTuple)
-            psTuple["NAME"] = psTuple["COMMAND"].split("/")[-1]
-            insertRow(psTuple)
+    #Refresh process table periodacally
+    #t = Thread(target=refreshTable, args=(1,))
+    #t.run()
+    t = RefresherThread(5)
+    t.start()
 
     window.show();
     sys.exit(app.exec_())
